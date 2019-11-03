513a9be3305bdbfa9c2243219668a44474e83822
Tuan Le <letuanlt09@gmail.com>
2019-11-03 10:57:53 -0500

    random display data, check the answers, update score and display

---
+++ Assets/data.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class data : MonoBehaviour
+{
+    //create data variable
+    private Dictionary<string, string> data1;
+    // input the data when getDate being called
+    public Dictionary<string, string> getData()
+        {
+            data1 = new Dictionary<string, string>();
+            data1.Add("Student", "A person who is studying at a school or college");
+            data1.Add("Teacher", "A person who teaches, especially in a school");
+            data1.Add("Lawyer", "A person who practices or studies law; an attorney or a counselor");
+            data1.Add("Doctor", "A qualified practitioner of medicine; a physician");
+
+            return data1;
+        }
+    // check if data
+    public bool checkData(string key, string value)
+    {
+        if (data1[key] == value)
+            return true;
+        else
+            return false;
+    }
+  
+}
---
+++ Assets/main.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+public class main : MonoBehaviour
+{
+   
+    // Create Key GameObject 
+    public GameObject[] key = new GameObject[4];
+    //Create Value GameObject
+    public GameObject[] value = new GameObject[4];
+    // Create Win-Text
+    public Text winText;
+    // Create score-text
+    public Text scoreText;
+    //Create Data
+    private Dictionary<string, string> data;
+
+    //random array
+    int[] arrRandKey;
+    int[] arrRandValue;
+
+    //isVisited array;
+    private Dictionary<string, bool> isVisited;
+
+
+
+    void Start()
+    {
+        //create arrRandKey
+        Random rand = new Random();
+        arrRandKey = new int[4];
+        int indexRand = 0;
+        while(indexRand<4)
+        {
+            int numRand = rand.Next(0, 4);
+            bool isGood = true;
+            for(int i = 0; i < indexRand;i++)
+            {
+                if(numRand == arrRandKey[i])
+                {
+                    isGood = false;
+                    break;
+                }
+            }
+            if(isGood)
+            {
+                arrRandKey[indexRand] = numRand;
+                indexRand++;
+            }
+        }
+        //create arrRandValue
+        arrRandValue = new int[4];
+        indexRand = 0;
+        while (indexRand < 4)
+        {
+            int numRand = rand.Next(0, 4);
+            bool isGood = true;
+            for (int i = 0; i < indexRand; i++)
+            {
+                if (numRand == arrRandValue[i])
+                {
+                    isGood = false;
+                    break;
+                }
+            }
+            if (isGood)
+            {
+                arrRandValue[indexRand] = numRand;
+                indexRand++;
+            }
+        }
+
+        //initialize isVisited
+        isVisited = new Dictionary<string, bool>();
+
+
+        // getData
+        data = this.gameObject.GetComponent<data>().getData();
+        //fetch data to Keys and Values
+        int index = 0;
+        foreach (KeyValuePair<string, string> i in data)
+        {
+            // input data to text
+            key[arrRandKey[index]].GetComponent<Text>().text = i.Key;
+            value[arrRandValue[index]].GetComponent<Text>().text = i.Value;
+            //update index
+            index++;
+            //add to isVisited
+            isVisited.Add(i.Key, false);
+        }
+        // display score
+        scoreText.text = "Score: " + score.ToString();
+
+        
+
+    }
+    // if state is onKey
+    bool onKey = false;
+    string selectedKey;
+    string selectedValue;
+    GameObject preKeyObj;
+    int score = 0;
+    public void onClicked(GameObject button)
+    {
+        //Debug.Log(button.GetComponentInChildren<Text>().text);
+        // change the highlightedColor when click on value
+        
+        if (Input.mousePosition.x < Screen.width/2 && isVisited[button.GetComponentInChildren<Text>().text] == false)
+        {
+            //set the highlightedColor
+            var color = button.GetComponent<Button>().colors;
+            color.highlightedColor = Color.blue;
+            button.GetComponent<Button>().colors = color;
+            //set onKey is true
+            onKey = true;
+            //set preKeyObj
+            preKeyObj = button;
+            //set selectedKey
+            selectedKey = button.GetComponentInChildren<Text>().text;
+
+
+
+        }
+        // when onKey is true and the click on value
+        if(Input.mousePosition.x > Screen.width/2 && onKey ==true)
+        {
+            
+            //update the selectedValue
+            selectedValue = button.GetComponentInChildren<Text>().text;
+            //check data
+            if(this.gameObject.GetComponent<data>().checkData(selectedKey,selectedValue))
+            {
+                //keep the blue color for normalColor of preKeyObj
+                var color = preKeyObj.GetComponent<Button>().colors;
+                color.normalColor = Color.blue;
+                preKeyObj.GetComponent<Button>().colors = color;
+                //change highlighedColor for the button 
+                color = button.GetComponent<Button>().colors;
+                color.highlightedColor = Color.blue;
+                button.GetComponent<Button>().colors = color;
+                //keep the blue color for normalColor of the button
+                color = button.GetComponent<Button>().colors;
+                color.normalColor = Color.blue;
+                button.GetComponent<Button>().colors = color;
+                //update the score
+                score++;
+                scoreText.text = "Score: " + score.ToString();
+                //call the winText
+                StartCoroutine(printWinText());
+                //check if done all 4
+                if (score == 4)
+                    winText.text = "WELL DONE";
+                //update isVisited
+                isVisited[preKeyObj.GetComponentInChildren<Text>().text] = true;
+                //reset onKey
+                onKey = false;
+
+
+
+            }
+        }
+
+    }
+
+    IEnumerator printWinText()
+    {
+        winText.GetComponent<Text>().text = "Correct!!";
+        yield return new WaitForSeconds(1);
+        winText.GetComponent<Text>().text = "";
+
+    }
+    void Update()
+    {
+        
+    }
+   
+}
